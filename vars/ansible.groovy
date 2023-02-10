1c01575ca1a87eb728d56348b49dce4d6c287f4d
akkyjain15 <106295658+akkyjain15@users.noreply.github.com>
2023-02-10 12:31:54 +0530

    Update ansible.groovy

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -6,24 +6,29 @@ def PlaybookExecution(){
  ansiblePlaybook credentialsId: 'assignment6-ssh', disableHostKeyChecking: true, installation: 'ansible1', inventory: 'inv', playbook: 'role_jenkins.yml'
 }
 
-def slackSend(){
-
-      aborted {
-              slackSend channel: '#jenkinscicd',
-              color: 'danger',
-              message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
-              teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'
-      }          
-      success {
-              slackSend channel: '#jenkinscicd',
-              color: 'good',
-              message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
-              teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'
-      }
-      failure {
-              slackSend channel: '#jenkinscicd',
-              color: 'danger',
-              message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
-              teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'
-      }
+def call(String buildResult) {
+  if ( buildResult == "SUCCESS" ) {
+    slackSend channel: '#jenkinscicd',
+    color: 'good',
+    message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+    teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'         
+  }
+  else if( buildResult == "FAILURE" ) { 
+    slackSend channel: '#jenkinscicd',
+    color: 'danger',
+    message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+    teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'      
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend channel: '#jenkinscicd',
+    color: 'warining',
+    message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+    teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'      
+  }
+  else {
+    slackSend channel: '#jenkinscicd',
+    color: 'danger',
+    message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+    teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'      
+  }
 }
