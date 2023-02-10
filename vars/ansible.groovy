953b046f7c36d59e599f9b657799a40f94e58000
akkyjain15 <106295658+akkyjain15@users.noreply.github.com>
2023-02-10 11:43:09 +0530

    Create ansible.groovy

---
+++ vars/ansible.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone') {
+            steps {
+                git branch: 'main', url: 'https://gitlab.com/Akshay15jain/ansible.git'
+            }
+        }
+        stage('Playbook_Execution') {
+            input {
+              message 'Want to execute playbook?'
+            }
+
+            steps {
+                ansiblePlaybook credentialsId: 'assignment6-ssh', disableHostKeyChecking: true, installation: 'ansible1', inventory: 'inv', playbook: 'role_jenkins.yml'
+            }
+        }
+    }    
+    post {
+      aborted {
+              slackSend channel: '#jenkinscicd',
+              color: 'danger',
+              message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+              teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'
+      }          
+      success {
+              slackSend channel: '#jenkinscicd',
+              color: 'good',
+              message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+              teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'
+      }
+      failure {
+              slackSend channel: '#jenkinscicd',
+              color: 'danger',
+              message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+              teamDomain: 'ninja-gjj9738', tokenCredentialId: 'slack'
+      }
+    }
+    
+}
